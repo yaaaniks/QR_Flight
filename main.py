86a59aee0091285237a0be6bef2113d874a537f3
Георгий Макаров <makarov.dslt@gmail.com>
2022-11-18 17:47:09 +0300

    Code

--- main.py
+++ main.py
@@ -7,44 +7,42 @@ import os
 
 # Функция, которая сканирует QR-код и возвращает команды дрону для передвижения и выполнения задания. Функция
 # обрабатывает только QR-коды в формате, указанном в инструкции.
-def read_qr(camera_frame):
-    task_x = 0
-    task_y = 0
-    task_num = 0  # переменной task_num будет присваиваться номер задания, 1, 2 или 3
-    rgb = [0, 0, 0]
-    finish = False  # будет True, если дрон получит команду о завершении скрипта
+def read_qr(camera_frame, drone):
+    t1_x = 0
+    t1_y = 0
+    t2_x = 0
+    t2_y = 0
+    t3_x = 0
+    t3_y = 0
+    shift = 0
     try:
-        gray = cv2.cvtColor(camera_frame, cv2.COLOR_BGR2GRAY)  # кадр переводим в серый формат
+        gray = cv2.cvtColor(camera_frame, cv2.COLOR_BGR2GRAY)
         detector = cv2.QRCodeDetector()
         # встроенная функция библиотеки OpenCV, возвращает зашифрованный текст в виде строки
         string, _, _ = detector.detectAndDecode(gray)
-        if '0' in string:  # проверка на то, что QR-код обработан (в нём должен находится символ 0)
+        if '.' in string:  # проверка на то, что QR-код обработан
             text = string.split()  # создаём список, содержащий каждое из полученных чисел в отдельности
-            print("[INFO] Получен следующий текст: ", text)
+            print("[INFO] Получен набор команд: ", text)
             com_x = float(text[0])  # присваиваем x координату, к которой полетит дрон после выполнения задания
             com_y = float(text[1])  # присваиваем y координату, к которой полетит дрон после выполнения задания
             # если обе координаты равны нулю, дрон выполнит задание и сядет, для этого задействуем флаг finish
             if com_x == 0 and com_y == 0:
-                print('[INFO] Получен код, соответствующей посадке')
-                finish = True
-            if int(text[2]) == 1:  # если в 3 элементе стоит 1, то дрон получает координаты места для фотографии
-                task_x = float(text[5])
-                task_y = float(text[6])
-                task_num = 1
-            if int(text[3]) == 1:  # если в 4 элементе стоит 1, то дрон получает координаты места для посадки и взлёта
-                task_x = float(text[5])
-                task_y = float(text[6])
-                task_num = 2
-            # если в 5 элементе стоит 1, то дрон получает 3 числа, которые впоследствии зажгут светодиод в определённом
-            # цвете, числа сохраняются в массиве rgb
-            if int(text[4]) == 1:
-                rgb = [int(text[7]), int(text[8]), int(text[9])]
-                task_num = 3
-            return com_x, com_y, task_x, task_y, rgb, task_num, finish
-        return float(0), float(0), task_x, task_y, rgb, task_num, finish
+                print('[INFO] Дрон ')
+                shift = int(text[2])
+            else:
+                t1_x = float(text[3])
+                t1_y = float(text[4])
+                t2_x = float(text[5])
+                t2_y = float(text[6])
+                t3_x = float(text[7])
+                t3_y = float(text[8])
+            return com_x, com_y, t1_x, t1_y, t2_x, t2_y, t3_x, t3_y, shift
+        return float(0), float(0), t1_x, t1_y, t2_x, t2_y, t3_x, t3_y, shift
     except cv2.error:  # если возникла ошибка, возвращает нули и finish = False, т.е. не отдаёт команд
-        return float(0), float(0), task_x, task_y, rgb, task_num, finish
+        return float(0), float(0), t1_x, t1_y, t2_x, t2_y, t3_x, t3_y, shift
 
+
+def letter_define shdh
 
 # Функция, обрабатывающая команды с QR-кодов. Она циклично получает с камеры кадры и сканирует их на наличие
 # зашифрованных команд. После получения команд, выполняет их.
@@ -73,7 +71,7 @@ def drone_flight(drone, camera_ip):
                 # task_progress определяет, находится ли дополнительное здание в процессе выполнения, чтобы не
                 # прерывать основное, False - дрон не получил задание и готов сканировать QR-код
                 if task_progress is False:
-                    new_x, new_y, task_x, task_y, rgb, task_num, finished = read_qr(camera_frame)
+                    new_x, new_y, task_x, task_y, rgb, task_num, finished = read_qr(camera_frame, drone)
                     finish = finished  # извлекаем флаг, соответствующей завершению задания
                     # если получена команда сфотографировать или приземлиться, дрон вначале переместится к координатам,
                     # в которых нужно выполнить задание
@@ -81,10 +79,7 @@ def drone_flight(drone, camera_ip):
                         drone.go_to_local_point(x=(command_x+task_x), y=(command_y+task_y), z=flight_height, yaw=0)
                         task_progress = True
                     elif task_num == 3:  # в случае включения светодиода никуда перемещаться не надо
-                        print("[INFO] Получена команда включить светодиод")
-                        pioneer_mini.led_control(r=rgb[0], g=rgb[1], b=rgb[2])  # извлекаем значения RGB из массива
-                        time.sleep(3)
-                        pioneer_mini.led_control(r=0, g=0, b=0)
+
                         new_point = True
                         # обновление координат, new_x и new_y - положение следующего QR-кода относительно прошлого
                         command_x += new_x
